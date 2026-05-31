ab3adad46ba5b95d56fbe5cfb29e4d66dd928fab
EVIL BOY XD <134475157+sahilkumar210810@users.noreply.github.com>
2026-06-01 04:04:14 +0530

    Update start.py

--- AnnieXMedia/utils/inline/start.py
+++ AnnieXMedia/utils/inline/start.py
@@ -26,7 +26,7 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_7"], user_id=config.OWNER_ID),
+            #InlineKeyboardButton(text=_["S_B_7"], user_id=config.OWNER_ID),
             InlineKeyboardButton(text=_["S_B_4"], url=config.SUPPORT_CHAT),
         ],
         [
